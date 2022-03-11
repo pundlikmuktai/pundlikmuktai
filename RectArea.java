150a71a071a2fbf0d7024c94ef09d73d264e81b7
pundlikmuktai <101397814+pundlikmuktai@users.noreply.github.com>
2022-03-11 16:07:23 +0530

    Add files via upload

---
+++ RectArea.java
@@ -0,0 +1,30 @@
+    class rectangle
+{
+     int, length,width;
+    void getdata(inta, intb)
+{
+    length=a;
+   width=b;
+ }
+     int rectangle()
+  {
+      int area=length* width;
+    return(area);
+}
+}
+     class RectArea
+{
+    public static void main(String args[])
+{
+    int area1,area2;
+    Rectangle Rec1=new Rectangle();
+    Rectangle Rec2=new Rectangle();
+     Rec1.length=15;
+    Rec1.width=11;
+    area2=rec1,length*Rec1.width;
+     Rec2,getdata(20,15);
+    area2=Rec2 Recarea()
+    System.out println("Area="+area1);
+    System.out println("Area="+area2);
+}
+}
